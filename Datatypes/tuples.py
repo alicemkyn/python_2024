1814abe6ddad4d0d3c7fc497deea79c25ff97304
alicemkyn <alicemkoyun@gmail.com>
2024-02-08 18:08:13 +0300

    Tuple[Datatypes] added.

---
+++ Datatypes/tuples.py
@@ -0,0 +1,16 @@
+################################ TUPLE #################################
+'''
+- Immutable 
+- Ordered
+- Indexable
+- Can contain duplicates
+'''
+
+# METHODS
+# index()
+tup = ('apple',)
+print(tup.index('apple')) # 0
+
+# count()
+tup = ('apple', 'pear', 'apple')
+print(tup.count('apple')) # 2
